0993a8a0bd0d868e2b2ea7fc8cd0d45bbe83aa67
Nathan Prat <nathan.prat@gmail.com>
2015-11-19 17:37:14 +0100

    Initial commit

---
+++ Arma_db.cpp
@@ -0,0 +1,6 @@
+// Arma_db.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+
---
+++ dllmain.cpp
@@ -0,0 +1,29 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "stdafx.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+// test ingame : hint ("Arma_db" callExtension "");
+extern "C"
+{
+	__declspec (dllexport) void __stdcall RVExtension(char *output, int outputSize, const char *function);
+}
+
+void __stdcall RVExtension(char *output, int outputSize, const char *function)
+{
+	strncpy_s(output, outputSize, "IT WORKS!", _TRUNCATE);
+}
